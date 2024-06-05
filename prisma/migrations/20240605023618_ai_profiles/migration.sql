44d7c57eca8510286a67bfe16c1e67d6144ec10e
Ben Foden <ben.foden@gmail.com>
2024-06-05 11:38:34 +0900

    upate to add ai personas

---
+++ prisma/migrations/20240605023618_ai_profiles/migration.sql
@@ -0,0 +1,43 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `coachName` on the `Comment` table. All the data in the column will be lost.
+  - You are about to drop the column `coachVariant` on the `Comment` table. All the data in the column will be lost.
+  - Added the required column `createdByPersonaId` to the `Comment` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateTable
+CREATE TABLE "Persona" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "image" TEXT,
+    "age" INTEGER,
+    "gender" TEXT,
+    "occupation" TEXT,
+    "traits" TEXT NOT NULL,
+    "communicationStyle" TEXT,
+    "communicationSample" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "createdById" TEXT NOT NULL,
+    CONSTRAINT "Persona_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Comment" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "content" TEXT NOT NULL,
+    "isAI" BOOLEAN NOT NULL DEFAULT true,
+    "createdByPersonaId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "postId" TEXT NOT NULL,
+    CONSTRAINT "Comment_createdByPersonaId_fkey" FOREIGN KEY ("createdByPersonaId") REFERENCES "Persona" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Comment" ("content", "createdAt", "id", "isAI", "postId") SELECT "content", "createdAt", "id", "isAI", "postId" FROM "Comment";
+DROP TABLE "Comment";
+ALTER TABLE "new_Comment" RENAME TO "Comment";
+PRAGMA foreign_key_check("Comment");
+PRAGMA foreign_keys=ON;
