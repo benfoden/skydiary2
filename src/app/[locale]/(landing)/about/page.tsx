bee30e625a1495857283de2fcb7bb90bd823e14e
ben foden <ben.foden@gmail.com>
2024-06-20 08:18:39 +0900

    hmm

--- src/app/[locale]/(landing)/about/page.tsx
+++ src/app/[locale]/(landing)/about/page.tsx
@@ -1,34 +1,26 @@
+import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 import Button from "~/app/_components/Button";
 
 export default async function About() {
+  const t = await getTranslations();
   return (
     <>
       <main className="mt-[-72px] flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center px-4 py-8">
           <div className="flex flex-col gap-4">
-            <p className="text-xl">about skydiary</p>
-            <p>the benefits of journaling are countless</p>
-            <p>normally the process of writing is solitary and private</p>
-            <p>skydiary is a private space, but not so solitary</p>
-            <p>
-              with the help of AI, you get totally private advice and counsel
-            </p>
-            <p>
-              the emphasis is on simplicity. all you need to do is write a daily
-              entry
-            </p>
-            <p>
-              the AI assistant will tag your entry, recap your week, and give
-              advice when you want it
-            </p>
-            <p>
-              you get feedback on your progress over time and learn new things
-              you might not have imagined
-            </p>
-            <Link href="/auth/signin">
-              <Button variant="cta">try it</Button>
-            </Link>
+            <p className="text-xl">{t("about.title")}</p>
+            <p>{t("about.description 0")}</p>
+            <p>{t("about.description 1")}</p>
+            <p>{t("about.description 2")}</p>
+            <p>{t("about.description 3")}</p>
+            <p>{t("about.description 4")}</p>
+            <p>{t("about.description 5")}</p>
+            <div className="pt-8">
+              <Link href="/auth/signin">
+                <Button variant="cta">{t("about.try it")}</Button>
+              </Link>
+            </div>
           </div>
         </div>
       </main>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,58 +1,12 @@
-import { withAuth } from "next-auth/middleware";
-import createIntlMiddleware from "next-intl/middleware";
-import { type NextRequest } from "next/server";
+import createMiddleware from "next-intl/middleware";
 import { defaultLocale, locales } from "./i18n-config";
 
-const publicPages = [
-  "/",
-  "/auth/signin",
-  "/auth/signout",
-  "/auth/error",
-  "/about",
-  // (/secret requires auth)
-];
-
-const intlMiddleware = createIntlMiddleware({
+export default createMiddleware({
   locales,
-  localePrefix: "as-needed",
   defaultLocale,
+  localePrefix: "as-needed",
 });
 
-const authMiddleware = withAuth(
-  // Note that this callback is only invoked if
-  // the `authorized` callback has returned `true`
-  // and not for pages listed in `pages`.
-  (req) => intlMiddleware(req),
-  {
-    callbacks: {
-      authorized: ({ token }) => token != null,
-    },
-    pages: {
-      signIn: "/auth/signin",
-      signOut: "/auth/signout",
-      error: "/auth/error",
-      newUser: "/auth/new-user",
-    },
-  },
-);
-
-export default function middleware(req: NextRequest) {
-  const publicPathnameRegex = RegExp(
-    `^(/(${locales.join("|")}))?(${publicPages
-      .flatMap((p) => (p === "/" ? ["", "/"] : p))
-      .join("|")})/?$`,
-    "i",
-  );
-  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
-
-  if (isPublicPage) {
-    return intlMiddleware(req);
-  } else {
-    return (authMiddleware as any)(req);
-  }
-}
-
 export const config = {
-  // Skip all paths that should not be internationalized
-  matcher: ["/((?!api|_next|.*\\..*).*)"],
+  matcher: ["/", "/(ja|en)/:path*", "/((?!_next|_vercel|.*\\..*).*)"],
 };
