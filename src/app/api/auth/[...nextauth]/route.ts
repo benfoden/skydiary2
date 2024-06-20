8857bc3e4695d9385eb18413f770cb2d289c0f03
Ben Foden <ben.foden@gmail.com>
2024-06-20 11:22:52 +0900

    revert auth stuf

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,7 @@
 import NextAuth from "next-auth";
-import { getTranslations } from "next-intl/server";
+
 import { authOptions } from "~/server/auth";
-import { type EmailDetails } from "~/utils/types";
-const getStuff = async () => {
-  const t = await getTranslations();
 
-  const emailDetails: EmailDetails = {
-    text: t("email.auth.text"),
-    subject: t("email.auth.subject"),
-    body: t("email.auth.body"),
-    code: t("email.auth.code"),
-    goBack: t("email.auth.go back"),
-    safelyIgnore: t("email.auth.safely ignore it"),
-  };
-
-  // Insert any logic here before NextAuth is called
-
-  return authOptions(emailDetails);
-};
-
-const options = await getStuff();
-const handler = () => NextAuth(options);
-
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
