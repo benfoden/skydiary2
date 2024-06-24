d9b0e185e590f78c7a499b5fd89dbd0081165c8c
Ben Foden <ben.foden@gmail.com>
2024-06-24 09:56:30 +0900

    adjusting

--- src/app/(session)/entry/[id]/EntryBody.tsx
+++ src/app/(session)/entry/[id]/EntryBody.tsx
@@ -72,7 +72,7 @@ export default function EntryBody({ post }: { post: Post }) {
   }, []);
 
   return (
-    <div className="flex h-full w-full flex-col items-center gap-12 px-4 pb-4">
+    <div className="flex h-full w-full flex-col items-center gap-12 pb-4">
       <textarea
         ref={textareaRef}
         value={content}
@@ -82,7 +82,7 @@ export default function EntryBody({ post }: { post: Post }) {
             ? t("status.loading")
             : t("entry.today")
         }
-        className="min-h-[calc(100vh-224px)] w-full resize-none rounded-3xl border-none bg-white/20 px-8 py-4 text-[#424245] focus:outline-none active:text-[#424245] sm:max-w-5xl sm:px-16 sm:py-12"
+        className="min-h-full w-full resize-none rounded-xl border-none bg-white/20 px-8 py-6 text-[#424245] focus:outline-none active:text-[#424245] sm:max-w-5xl sm:rounded-3xl sm:px-16 sm:py-12"
         autoFocus
         style={{ height: "auto", overflow: "hidden", paddingBottom: "16px" }}
         onInput={adjustTextareaHeight}
--- src/app/(session)/entry/[id]/page.tsx
+++ src/app/(session)/entry/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use server";
-import { type Persona } from "@prisma/client";
+import { type Persona, type Tag } from "@prisma/client";
 import {
   ChatBubbleIcon,
   DotsHorizontalIcon,
@@ -12,7 +12,6 @@ import { revalidatePath } from "next/cache";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import { Suspense } from "react";
 import Button from "~/app/_components/Button";
 import { Card } from "~/app/_components/Card";
 import CopyTextButton from "~/app/_components/CopyTextButton";
@@ -22,7 +21,6 @@ import FormButton from "~/app/_components/FormButton";
 import { NavChevronLeft } from "~/app/_components/NavChevronLeft";
 import { PersonaIcon } from "~/app/_components/PersonaIcon";
 import { SessionNav } from "~/app/_components/SessionNav";
-import Spinner from "~/app/_components/Spinner";
 import { getUserLocale } from "~/i18n";
 import { getResponse, getResponseJSON } from "~/server/api/ai";
 import { api } from "~/trpc/server";
@@ -80,108 +78,94 @@ export default async function Entry({
           </form>
         </DropDownMenu>
       </SessionNav>
-      <div className="flex h-full flex-col items-center px-4 pb-4">
-        <Suspense
-          fallback={
-            <div className="flex h-full w-full items-center justify-center">
-              <Spinner />
-            </div>
-          }
-        >
-          <EntryBody post={post} />
-        </Suspense>
+      <div className="flex h-full flex-col items-center px-2 pb-4 sm:px-8">
+        <EntryBody post={post} />
         <div className="flex w-full max-w-5xl flex-col items-center gap-4">
           <div className="flex w-full flex-row items-center justify-center gap-4">
-            <form
-              action={async () => {
-                "use server";
-                try {
-                  if (searchParams.s === "1") {
-                    return;
-                  }
-                  const latestPost = await api.post.getByPostId({
-                    postId: params.id,
-                  });
-
-                  const tags = await getResponse(
-                    generateTagsPrompt + latestPost?.content,
-                  );
-
-                  const tagContents = tags?.split(",").map((tag) => tag.trim());
-                  const tagIds = tagContents
-                    ?.map((content) => {
-                      const tag = TAGS.find((tag) => tag.content === content);
-                      return tag?.id ?? undefined;
-                    })
-                    .filter((tag): tag is string => tag !== undefined);
-                  if (tagIds?.length) {
-                    await api.post.addTags({
-                      postId: params?.id,
-                      tagIds: tagIds,
+            <ul className="flex flex-row flex-wrap gap-2">
+              {tags?.map((tag: Tag) => (
+                <li key={tag.id}>
+                  <Link href={`/topics/${tag.content}/${tag.id}`}>
+                    <Button variant="chip">{tag.content}</Button>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <div className="flex w-full flex-row items-center justify-end gap-2">
+              <form
+                action={async () => {
+                  "use server";
+                  try {
+                    if (searchParams.s === "1") {
+                      return;
+                    }
+                    const latestPost = await api.post.getByPostId({
+                      postId: params.id,
                     });
-                  } else {
-                    console.error("Failed to tag.");
-                  }
 
-                  const latestPersona = await api.persona.getUserPersona();
-                  const generatedPersona = await getResponseJSON(
-                    generatePersonaPrompt(latestPersona ?? NEWPERSONAUSER) +
-                      latestPost?.content,
-                  );
+                    const tags = await getResponse(
+                      generateTagsPrompt + latestPost?.content,
+                    );
 
-                  if (typeof generatedPersona === "string") {
-                    const personaObject = JSON.parse(
-                      generatedPersona,
-                    ) as Persona;
-                    await api.persona.update({
-                      personaId: latestPersona?.id ?? "",
-                      name: latestPersona?.name ?? "",
-                      description: personaObject?.description ?? "",
-                      image: latestPersona?.image ?? "",
-                      age: personaObject?.age ?? 0,
-                      gender: personaObject?.gender ?? "",
-                      relationship: personaObject?.relationship ?? "",
-                      occupation: personaObject?.occupation ?? "",
-                      traits: personaObject?.traits ?? "",
-                      communicationStyle:
-                        personaObject?.communicationStyle ?? "",
-                      communicationSample:
-                        personaObject?.communicationSample ?? "",
-                    });
+                    const tagContents = tags
+                      ?.split(",")
+                      .map((tag) => tag.trim());
+                    const tagIds = tagContents
+                      ?.map((content) => {
+                        const tag = TAGS.find((tag) => tag.content === content);
+                        return tag?.id ?? undefined;
+                      })
+                      .filter((tag): tag is string => tag !== undefined);
+                    if (tagIds?.length) {
+                      await api.post.addTags({
+                        postId: params?.id,
+                        tagIds: tagIds,
+                      });
+                    } else {
+                      console.error("Failed to tag.");
+                    }
+
+                    const latestPersona = await api.persona.getUserPersona();
+                    const generatedPersona = await getResponseJSON(
+                      generatePersonaPrompt(latestPersona ?? NEWPERSONAUSER) +
+                        latestPost?.content,
+                    );
+
+                    if (typeof generatedPersona === "string") {
+                      const personaObject = JSON.parse(
+                        generatedPersona,
+                      ) as Persona;
+                      await api.persona.update({
+                        personaId: latestPersona?.id ?? "",
+                        name: latestPersona?.name ?? "",
+                        description: personaObject?.description ?? "",
+                        image: latestPersona?.image ?? "",
+                        age: personaObject?.age ?? 0,
+                        gender: personaObject?.gender ?? "",
+                        relationship: personaObject?.relationship ?? "",
+                        occupation: personaObject?.occupation ?? "",
+                        traits: personaObject?.traits ?? "",
+                        communicationStyle:
+                          personaObject?.communicationStyle ?? "",
+                        communicationSample:
+                          personaObject?.communicationSample ?? "",
+                      });
+                    }
+                  } catch (error) {
+                    console.error("Error creating tags:", error);
+                  } finally {
+                    if (!error) revalidatePath(`/entry/${params.id}`);
                   }
-                } catch (error) {
-                  console.error("Error creating tags:", error);
-                } finally {
-                  if (!error) revalidatePath(`/entry/${params.id}`);
-                }
-              }}
-            >
-              <FormButton isDisabled={searchParams.s === "1"}>
-                <FrameIcon className="h-5 w-5" />
-              </FormButton>
-            </form>
-            <Suspense
-              fallback={
-                <div className="flex h-full w-full items-center justify-center">
-                  <Spinner />
-                </div>
-              }
-            >
-              {tags && (
-                <ul className="flex flex-row gap-2">
-                  {tags.map((tag) => (
-                    <li key={tag.id}>
-                      <Link href={`/topics/${tag.content}/${tag.id}`}>
-                        <Button variant="chip">{tag.content}</Button>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </Suspense>
+                }}
+              >
+                <FormButton isDisabled={searchParams.s === "1"}>
+                  <FrameIcon className="h-5 w-5" />
+                </FormButton>
+              </form>
+            </div>
           </div>
           <div className="flex h-full w-full flex-col items-center pb-4">
-            <div className="flex w-full flex-row items-center justify-center gap-2">
+            <div className="flex w-full flex-row items-start justify-end gap-2 pb-4">
               <form
                 action={async () => {
                   "use server";
@@ -232,123 +216,118 @@ export default async function Entry({
                   <ChatBubbleIcon className="h-5 w-5" />
                 </FormButton>
               </form>
-              {personas.map((persona) => (
-                <form
-                  key={persona.id}
-                  action={async () => {
-                    "use server";
-                    if (searchParams.s === "1") {
-                      return;
-                    }
-                    try {
-                      const latestPost = await api.post.getByPostId({
-                        postId: params.id,
-                      });
-
-                      const currentUserPersona =
-                        await api.persona.getUserPersona();
-
-                      let updatedContent = "";
-                      if (currentUserPersona) {
-                        updatedContent =
-                          latestPost?.content +
-                          "End of journal entry. When writing your response, also consider that this journal entry was written by the following person: " +
-                          JSON.stringify(currentUserPersona);
-                      }
-
-                      const response = await getResponse(
-                        personaPrompt(persona) + updatedContent ??
-                          latestPost?.content,
-                      );
-                      if (response) {
-                        await api.comment.create({
-                          content: response,
-                          postId: params?.id,
-                          createdByPersonaId: persona.id,
-                          coachVariant: persona.name,
-                        });
-                        revalidatePath(`/entry/${params.id}`);
-                      } else {
-                        console.error(
-                          "Failed to get a response for the comment.",
-                        );
-                      }
-                    } catch (error) {
-                      console.error("Error creating comment:", error);
-                    }
-                  }}
-                >
-                  <FormButton isDisabled={searchParams.s === "1"}>
-                    <div className="flex flex-row items-center gap-2 font-medium">
-                      {persona.image ? (
-                        <>
-                          <Image
-                            alt={persona.name}
-                            src={persona.image}
-                            width="16"
-                            height="16"
-                            className="rounded-full"
-                          />
-                          <span className="text-xs">{persona.name}</span>
-                        </>
-                      ) : (
-                        <>
-                          <PersonIcon className="h-4 w-4" />
-                          <span className="text-xs">{persona.name}</span>
-                        </>
-                      )}
-                    </div>
-                  </FormButton>
-                </form>
-              ))}
               <Link href="/persona/all">
                 <Button>
-                  <DotsHorizontalIcon className="h-4 w-4" />
+                  <DotsHorizontalIcon className="h-5 w-5" />
                 </Button>
               </Link>
             </div>
-            <Suspense
-              fallback={
-                <div className="flex h-full w-full items-center justify-center font-light">
-                  <Spinner />
-                </div>
-              }
-            >
-              {comments && (
-                <ul className="w-[420px]">
-                  {comments
-                    .sort(
-                      (a, b) => b.createdAt.getTime() - a.createdAt.getTime(),
-                    )
-                    .map((comment) => (
-                      <li
-                        key={comment.id}
-                        className="flex flex-col rounded-lg p-4"
-                      >
-                        <Card isButton={false}>
-                          <div className="flex w-full flex-col gap-4 py-4">
-                            <div className="flex w-full justify-between gap-4 text-xs">
-                              <div className="font-medium">
-                                <PersonaIcon
-                                  personaId={comment.createdByPersonaId!}
-                                  personas={personas}
-                                  coachVariant={comment.coachVariant ?? ""}
-                                />
-                              </div>
-                              <div>
-                                {formattedTimeStampToDate(comment.createdAt)}
-                              </div>
+            <div className="flex w-full flex-row items-start justify-center gap-2">
+              <ul className="flex flex-row flex-wrap justify-start gap-2">
+                {personas.map((persona: Persona) => (
+                  <form
+                    key={persona.id}
+                    action={async () => {
+                      "use server";
+                      if (searchParams.s === "1") {
+                        return;
+                      }
+                      try {
+                        const latestPost = await api.post.getByPostId({
+                          postId: params.id,
+                        });
+
+                        const currentUserPersona =
+                          await api.persona.getUserPersona();
+
+                        let updatedContent = "";
+                        if (currentUserPersona) {
+                          updatedContent =
+                            latestPost?.content +
+                            "End of journal entry. When writing your response, also consider that this journal entry was written by the following person: " +
+                            JSON.stringify(currentUserPersona);
+                        }
+
+                        const response = await getResponse(
+                          personaPrompt(persona) + updatedContent ??
+                            latestPost?.content,
+                        );
+                        if (response) {
+                          await api.comment.create({
+                            content: response,
+                            postId: params?.id,
+                            createdByPersonaId: persona.id,
+                            coachVariant: persona.name,
+                          });
+                          revalidatePath(`/entry/${params.id}`);
+                        } else {
+                          console.error(
+                            "Failed to get a response for the comment.",
+                          );
+                        }
+                      } catch (error) {
+                        console.error("Error creating comment:", error);
+                      }
+                    }}
+                  >
+                    <FormButton isDisabled={searchParams.s === "1"}>
+                      <div className="flex flex-row items-center gap-2 font-medium">
+                        {persona.image ? (
+                          <>
+                            <Image
+                              alt={persona.name}
+                              src={persona.image}
+                              width="16"
+                              height="16"
+                              className="rounded-full"
+                            />
+                            <span className="text-xs">{persona.name}</span>
+                          </>
+                        ) : (
+                          <>
+                            <PersonIcon className="h-4 w-4" />
+                            <span className="text-xs">{persona.name}</span>
+                          </>
+                        )}
+                      </div>
+                    </FormButton>
+                  </form>
+                ))}
+              </ul>
+            </div>
+
+            {comments && (
+              <ul className="w-[420px]">
+                {comments
+                  .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+                  .map((comment) => (
+                    <li
+                      key={comment.id}
+                      className="flex flex-col rounded-lg p-4"
+                    >
+                      <Card isButton={false}>
+                        <div className="flex w-full flex-col gap-4 py-4">
+                          <div className="flex w-full justify-between gap-4 text-xs">
+                            <div className="font-medium">
+                              <PersonaIcon
+                                personaId={comment.createdByPersonaId!}
+                                personas={personas}
+                                coachVariant={comment.coachVariant ?? ""}
+                              />
                             </div>
-                            <div className="max-w-md text-sm">
-                              {comment.content}
+                            <div>
+                              {formattedTimeStampToDate(comment.createdAt)}
                             </div>
                           </div>
-                        </Card>
-                      </li>
-                    ))}
-                </ul>
-              )}
-            </Suspense>
+                          <div className="max-w-md text-sm">
+                            {comment.content}
+                          </div>
+                        </div>
+                      </Card>
+                    </li>
+                  ))}
+              </ul>
+            )}
           </div>
         </div>
       </div>
